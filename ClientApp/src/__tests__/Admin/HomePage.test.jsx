67c4036f1fbc59859d46f689e1aa6b7b08b764da
Prasanna KUMAR <prasanna.kumar@axa-ppp.co.uk>
2019-07-04 15:43:07 +0100

    fixed home page tests(after cinnexting redux)

--- ClientApp/src/__tests__/Admin/HomePage.test.jsx
+++ ClientApp/src/__tests__/Admin/HomePage.test.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
-import {shallow} from '../../setupTests';
-import HomePage, { SideBar } from '../../components/Admin/HomePage';
+import { shallow } from '../../setupTests';
+import { _HomePage as HomePage, SideBar } from '../../components/Admin/HomePage';
 
 describe('Homepage test suite', () => {
 
     let component;
-    beforeEach(()=>{
-        component = shallow(<HomePage/>);
-    })
-    it('renders correctly',() => {
+    beforeEach(() => {
+        component = shallow(<HomePage />);
+    });
+
+    it('renders correctly', () => {
         expect(component).toHaveLength(1);
     });
 
-    it('renders sidebar',() => {
-        expect(component).toContainReact(<SideBar/>)
+    it('renders sidebar', () => {
+        expect(component).toContainReact(<SideBar />)
     });
 
 });
