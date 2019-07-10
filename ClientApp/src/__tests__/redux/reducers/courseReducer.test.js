94e2ad18faf91690e52768cdfc23ed05035078f8
Prasanna KUMAR <prasanna.kumar@axa-ppp.co.uk>
2019-07-10 09:10:46 +0100

    added course reducer

---
+++ ClientApp/src/__tests__/redux/reducers/courseReducer.test.js
@@ -0,0 +1,20 @@
+import courseReducer from '../../../redux/reducers/courseReducer'
+import ActionType from '../../../redux/ActionType';
+
+describe('posts reducer', () => {
+    it('should returns inital state', () => {
+        const newState = courseReducer(undefined, {});
+        expect(newState).toEqual([]);
+    });
+
+    it('shuold return new state when received an action type', () => {
+        let posts = [{ title: "test1" }, { title: "test2" }, { title: "test3" }, { title: "test4" }];
+
+        const newState = courseReducer(undefined, {
+            type: ActionType.CREATE_COURSE,
+            payload: posts
+        });
+        console.log(newState)
+        expect(newState).toEqual(posts);
+    });
+})
